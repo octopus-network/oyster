28038f8519c228560ef8f2e5ba9ac90df3b4c3f1
lesterli <fulfilself@gmail.com>
2023-07-06 10:54:31 +0000

    fix the evm issue

--- app/app.go
+++ app/app.go
@@ -352,7 +352,7 @@ func New(
 		appCodec,
 		keys[authtypes.StoreKey],
 		app.GetSubspace(authtypes.ModuleName),
-		authtypes.ProtoBaseAccount,
+		ethermint.ProtoAccount,
 		maccPerms,
 		sdk.Bech32PrefixAccAddr,
 	)
